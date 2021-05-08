ac0292fb18d231b9c2783cf726c22c573651b58d
canyie <a1364259@163.com>
2021-05-08 20:05:05 +0800

    Hide the modified traces of the flash_recovery service

--- template/service.sh
+++ template/service.sh
@@ -9,18 +9,45 @@ INITRC_NAME="init.rc"
 
 # Android 11's new init.rc
 [ -f "/init.rc" ] || INITRC_NAME="system/etc/init/hw/init.rc"
-INITRC="$MAGISK_TMP/.magisk/rootdir/$INITRC_NAME"
+
+INITRC="/$INITRC_NAME"
+MAGISKRC="$MAGISK_TMP/.magisk/rootdir/$INITRC_NAME"
 
 trim() {
-    trimmed=$1
-    trimmed=${trimmed%% }
-    trimmed=${trimmed## }
-    echo $trimmed
+  trimmed=$1
+  trimmed=${trimmed%% }
+  trimmed=${trimmed## }
+  echo $trimmed
+}
+
+# https://github.com/topjohnwu/Magisk/blob/master/native/jni/init/rootdir.cpp#L24
+grep_flash_recovery() {
+  # Some devices don't have the flash_recovery service
+  # (like Samsung renamed it to "ota_cleanup" but Magisk won't remove it, so we no need to do anything for this)
+  LINE=$(grep "service flash_recovery " "$INITRC") || return 1
+  LINE=${LINE#*"service flash_recovery "}
+  trim "$LINE"
+}
+
+reset_flash_recovery() {
+  FLASH_RECOVERY=$(grep_flash_recovery) || return
+
+  # Skip if the flash_recovery not removed by Magisk
+  grep -qxF "service flash_recovery /system/bin/xxxxx" "$MAGISKRC" || return
+
+  # Skip if the install-recovery.sh not exist
+  [ -f "$FLASH_RECOVERY" ] || return
+
+  # Skip if there is the state set for the service
+  [ "$(getprop 'init.svc.recovery' 2>/dev/null)" = "" ] || return
+
+  # Set a "fake" state for the service
+  resetprop 'init.svc.recovery' 'stopped'
 }
 
 grep_service_name() {
   ARG=$1
-  LINE=$(< "$INITRC" grep "service .* $MAGISK_TMP/magisk --$ARG")
+  LINE=$(grep "service .* $MAGISK_TMP/magisk --$ARG" "$MAGISKRC")
   LINE=${LINE#*"service "}
   LINE=${LINE%" $MAGISK_TMP"*}
   trim "$LINE"
@@ -30,14 +57,14 @@ del_service_name() {
   resetprop --delete "init.svc.$1"
 }
 
-hide_initrc() {
+delete_services() {
   # Wait for boot to complete
   while [ "$(getprop sys.boot_completed)" != "1" ]
   do
     sleep 1
   done
 
-  # Delete Magisk's service in system properties
+  # Remove Magisk's services' names from system properties
   POST_FS_DATA=$(grep_service_name "post-fs-data")
   LATE_START_SERVICE=$(grep_service_name "service")
   BOOT_COMPLETED=$(grep_service_name "boot-complete")
@@ -46,4 +73,5 @@ hide_initrc() {
   del_service_name "$BOOT_COMPLETED"
 }
 
-hide_initrc &
+reset_flash_recovery
+delete_services &
